83df3b3f464e876f401b271f62508c108b5415a7
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-03-28 20:30:12 -0500

    Add Data Annotations

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,11 +1,16 @@
-﻿namespace ApiProduct.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace ApiProduct.Models
 {
     public class Product
     {
+        [Key]
         public int ProductId { get; set; }
-        public string Name { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Range(1,1000)]
         public double Price { get; set; }
-        public string Description { get; set; }
-        public string ImageUrl { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
     }
 }
