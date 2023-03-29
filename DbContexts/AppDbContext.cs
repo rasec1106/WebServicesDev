ab7db48aa980cfa333be9f7675ad281e65e68e9f
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-03-28 20:54:52 -0500

    Add extra configurations to AppDbContext

---
+++ DbContexts/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ApiProduct.Models;
+using Microsoft.EntityFrameworkCore;
+namespace ApiProduct.DbContexts
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext()
+        {
+
+        }
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        // to autogenerate the tables we need to mention it into this class
+        public DbSet<Product> Products { get; set; }
+        // setting the default configuration file
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+        }
+    }
+}
