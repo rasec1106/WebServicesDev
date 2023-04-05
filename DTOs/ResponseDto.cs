d2d7082cfe523d0b53ed32c91046804d145636b0
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-04-04 19:54:50 -0500

    Add ResponseDto

---
+++ DTOs/ResponseDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ApiProduct.DTOs
+{
+    // This will help us to format the response
+    public class ResponseDto
+    {
+        public bool IsSucceed { get; set; }
+        public object? Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+    }
+}
