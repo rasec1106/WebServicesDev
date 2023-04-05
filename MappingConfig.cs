0f9b4b240df0db6717ff9ec4896a0fb1c413f506
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-04-04 20:33:38 -0500

    Add mapping config

---
+++ MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ApiProduct.DTOs;
+using ApiProduct.Models;
+using AutoMapper;
+namespace ApiProduct
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps()
+        {
+            var mappingConfig = new MapperConfiguration(config =>
+            {
+                // it maps the two classes by its fields. If they have a different name we can use a decorator (annotation)
+                // when i give you a ProductDto object it transforms it into a Product object
+                config.CreateMap<ProductDto, Product>();
+                // and viceversa
+                config.CreateMap<Product, ProductDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
