ea1387743f5cf6e52cd84c40913fad2547f07300
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-04-04 21:27:59 -0500

    Add ProductRepository and implement GetProducts

---
+++ Repository/ProductSQLRepository.cs
@@ -0,0 +1,46 @@
+﻿using ApiProduct.DbContexts;
+using ApiProduct.DTOs;
+using ApiProduct.Models;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiProduct.Repository
+{
+    public class ProductSQLRepository : IProductRepository
+    {
+        private AppDbContext dbContext;
+        private IMapper mapper;
+        // We will use dependency injection passing as parameters with the same name
+        public ProductSQLRepository(AppDbContext dbContext, IMapper mapper) 
+        {
+            this.dbContext = dbContext;
+            this.mapper = mapper;
+        }
+
+        public Task<ProductDto> CreateProduct(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ProductDto> GetProductById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            List<Product> products = await this.dbContext.Products.ToListAsync();
+            return this.mapper.Map<List<ProductDto>>(products);
+        }
+
+        public Task<ProductDto> UpdateProduct(ProductDto productDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
