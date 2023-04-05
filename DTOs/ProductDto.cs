9266284789c873778b9cb5ab218ebec7a89a998e
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-04-04 19:46:12 -0500

    Add productDTO

---
+++ DTOs/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ApiProduct.DTOs
+{
+    // We use this class to "transform" the object. DTO=Data Transformation Object
+    // e.g. we expose only some attributes of the model (we could also add some calculated properties to show) 
+    // in this case we don't show the price
+    public class ProductDto
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
