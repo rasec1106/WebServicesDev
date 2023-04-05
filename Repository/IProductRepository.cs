9ad31b57ae6c12079e7ef847d284415c0479235e
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-04-04 20:21:40 -0500

    Add IProductRepository

---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ApiProduct.DTOs;
+namespace ApiProduct.Repository
+{
+    public interface IProductRepository
+    {
+        public Task<IEnumerable<ProductDto>> GetProducts();
+        public Task<ProductDto> GetProductById(int productId);
+        public Task<ProductDto> CreateProduct(ProductDto productDto);
+        public Task<ProductDto> UpdateProduct(ProductDto productDto);
+        public Task<bool> DeleteProduct(int productId);
+    }
+}
