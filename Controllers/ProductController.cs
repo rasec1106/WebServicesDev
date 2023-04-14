de27890420c80a44e0eed45980544b807fc55b04
Cesar Herrera <Cesar.Herrera@tranzact.net>
2023-04-13 21:25:30 -0500

    Finish all http methods

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -17,7 +17,7 @@ namespace ApiProduct.Controllers
         }
 
         [HttpGet]
-        public async Task<Object> Get()
+        public async Task<object> Get()
         {
             try
             {
@@ -29,10 +29,87 @@ namespace ApiProduct.Controllers
             catch (Exception ex)
             {
                 this.responseDto.IsSucceed = false;
-                this.responseDto.Result = null;
+                // this.responseDto.Result = null;
                 this.responseDto.DisplayMessage = ex.ToString();
             }            
             return responseDto;            
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<object> Get(int id)
+        {
+            try
+            {
+                ProductDto productDto = await productRepository.GetProductById(id);
+                this.responseDto.IsSucceed = true;
+                this.responseDto.Result = productDto;
+                this.responseDto.DisplayMessage = "Success";
+            }
+            catch (Exception ex)
+            {
+                this.responseDto.IsSucceed = false;
+                // this.responseDto.Result = null;
+                this.responseDto.DisplayMessage = ex.ToString();
+            }
+            return responseDto;
+        }
+
+        [HttpPost]
+        public async Task<object> Post(ProductDto productDto)
+        {
+            try
+            {
+                ProductDto result = await productRepository.CreateProduct(productDto);
+                this.responseDto.IsSucceed = true;
+                this.responseDto.Result = result;
+                this.responseDto.DisplayMessage = "Success";
+            }
+            catch (Exception ex)
+            {
+                this.responseDto.IsSucceed = false;
+                // this.responseDto.Result = null;
+                this.responseDto.DisplayMessage = ex.ToString();
+            }
+            return responseDto;
+        }
+
+        [HttpPut]
+        public async Task<object> Put(ProductDto productDto)
+        {
+            try
+            {
+                ProductDto result = await productRepository.UpdateProduct(productDto);
+                this.responseDto.IsSucceed = true;
+                this.responseDto.Result = result;
+                this.responseDto.DisplayMessage = "Success";
+            }
+            catch (Exception ex)
+            {
+                this.responseDto.IsSucceed = false;
+                // this.responseDto.Result = null;
+                this.responseDto.DisplayMessage = ex.ToString();
+            }
+            return responseDto;
+        }
+
+        [HttpDelete]
+        public async Task<object> Delete(int id)
+        {
+            try
+            {
+                bool result = await productRepository.DeleteProduct(id);
+                this.responseDto.IsSucceed = true;
+                this.responseDto.Result = result;
+                this.responseDto.DisplayMessage = "Success";
+            }
+            catch (Exception ex)
+            {
+                this.responseDto.IsSucceed = false;
+                // this.responseDto.Result = null;
+                this.responseDto.DisplayMessage = ex.ToString();
+            }
+            return responseDto;
         }
     }
 }
